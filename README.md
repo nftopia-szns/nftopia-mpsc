f4631e7bf489ce72933b81c81ebf1127d486df55
Phat Nguyen Luu <yadsendew@protonmail.com>
2022-07-15 02:33:42 +0000

    fix: hardhar pkg causes failed unit test

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Sample Hardhat Project
+# nftopia marketplace smart contracts
 
 This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
 
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,24 +1,56 @@
-require('dotenv').config();
+import * as dotenv from "dotenv";
+
+import "@nomicfoundation/hardhat-chai-matchers";
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-etherscan";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import { task } from "hardhat/config";
+
+dotenv.config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
 
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
     },
+    ropsten: {
+      url: process.env.ROPSTEN_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
     matic: {
       url: "https://polygon-rpc.com",
       chainId: 137,
       gasPrice: 50000000000,
-      accounts: [process.env.PRIVATE_KEY]
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     mumbai: {
       url: "https://matic-mumbai.chainstacklabs.com",
       chainId: 80001,
       gasPrice: 20000000000,
-      accounts: [process.env.PRIVATE_KEY]
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     }
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
   },
   etherscan: {
     apiKey: process.env.POLYGONSCAN_API_KEY
